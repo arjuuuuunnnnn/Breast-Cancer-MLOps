f067171f320c707ea682f396c986f3c5d54f3ac1
arjuuuuunnnnn <arjunbanur27@gmail.com>
2024-04-15 17:40:48 +0530

    src/1 done

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -16,5 +16,39 @@ class DataPreProcessStrategy(DataStrategy):
     def handle_data(self, data:pd.DataFrame) -> pd.DataFrame:
 
         try:
-            data = data.drop([
-                ])
+            # any preprocessing like removing or adding columns are done here
+            # data = data.drop([])
+            return data
+        
+        except Exception as e:
+            logging.error(f"Error in preprocessing the data: {e}")
+            raise e
+
+class DataDivisionStrategy(DataStrategy):
+    def handle_data(self, data:pd.DataFrame) -> Union[pd.DataFrame, pd.Series]:
+        try:
+            X = data.drop(["diagnosis"], axis=1)
+            y = data["diagnosis"]
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+            return X_train, X_test, y_train, y_test
+        except Exception as e:
+            logging.error(f"Error in dividing data into train and test: {e}")
+            raise e
+
+class DataCleaning:
+
+    def __init__(self, data:pd.DataFrame, strategy: DataStrategy):
+        self.data = data
+        self.strategy = strategy
+
+    def handle_data(self) -> Union[pd.DataFrame, pd.Series]:
+        try:
+            return self.strategy.handle_data(self.data)
+        except Exception as e:
+            logging.error(f"Error in handling data: {e}")
+            raise e
+
+if __name__ == "__main__":
+    data = pd.read_csv("../data/data.csv")
+    data_cleaning = DataCleaning(data, DataPreProcessStrategy())
+    data_cleaning.handle_data()
