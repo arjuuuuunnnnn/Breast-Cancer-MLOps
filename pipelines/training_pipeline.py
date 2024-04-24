33e4d96a2787741bdbe45e39bcffafe886083dbb
arjuuuuunnnnn <arjunbanur27@gmail.com>
2024-04-24 11:10:38 +0530

    evaluation

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -2,6 +2,8 @@ from zenml import pipeline
 from steps.ingest_data import ingest_df
 from steps.clean_data import clean_df
 from steps.model_train import train_model
+from steps.evaluation import evaluate_model
+
 # chache is enabled coz if nothing changes in the data, the old data is used
 @pipeline(enable_cache=True)
 def train_pipeline(data_path:str):
