a78d0267afd889484b84106409e698aab3beb8f7
arjuuuuunnnnn <arjunbanur27@gmail.com>
2024-04-24 11:30:11 +0530

    inputs given

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -11,5 +11,5 @@ def train_pipeline(data_path:str):
     X_train, X_test, y_train, y_test = clean_df(df)
     model = train_model(X_train, X_test, y_train, y_test)
     r2, rmse = evaluate_model(model, X_test, y_test)
-    train_model(df)
-    evaluate_model(df)
+    train_model(X_train, X_test, y_train, y_test)
+    evaluate_model(X_train, X_test, y_train, y_test)
