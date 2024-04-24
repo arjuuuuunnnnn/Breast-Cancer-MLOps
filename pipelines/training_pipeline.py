a1c9ae71864f4df647dc3dfab4184feb7adfd487
arjuuuuunnnnn <arjunbanur27@gmail.com>
2024-04-24 11:04:18 +0530

    import error resolved

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -1,6 +1,6 @@
 from zenml import pipeline
 from steps.ingest_data import ingest_df
-
+from steps.clean_data import clean_df
 # chache is enabled coz if nothing changes in the data, the old data is used
 @pipeline(enable_cache=True)
 def train_pipeline(data_path:str):
